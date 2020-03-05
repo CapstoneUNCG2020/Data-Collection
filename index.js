338f3ec8ef71ea8119f8361e3ab09b6a3dbff81b
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2020-03-05 13:47:56 -0500

    Clean-up
    General Clean-up

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
 const express = require('express');
 const app = express();
-// const Datastore = require('nedb');
 const fetch = require('node-fetch');
 require('dotenv').config();
-const TournamentsForLeague = require('./tournamentsForLeague.js');
-const Players = require('./players.js');
-const tempPlayer = require('./tempPlayer.js');
 //New Imports
 const tournamentsForLeagueImpl = require('./implementation/tournamentsForLeagueImpl');
 const scheduleForLeagueImpl = require('./implementation/scheduleForLeague');
 const standingsAndPlayersForTournamentImpl = require('./implementation/standingsAndPlayersForTournament');
-// const Car = require('./car.js');
-
-// myCar = new Car();
-// console.log(myCar.Year + " " + myCar.Type);
-
-// console.log('split');
-
-// myCar.Year = 2000;
-// console.log(myCar.Year);
-
-// console.log();
-
-// console.log(process.env);
 
 const api_key = process.env.API_KEY;
-
-// const database = new Datastore('database.db');
-// database.loadDatabase();
 
 app.listen(3000, () => console.log('listening at 3000'));
 
 var headers = {
     "x-api-key" : api_key
 }
-
-//testingAPI(headers);
-
 
 // ID's of the leagues which are seperated by regions and worlds.
 leagueId = [
@@ -67,17 +44,9 @@ tournamentsForLeague = new tournamentsForLeagueImpl();
     standingsAndPlayersForTournament.getStandingsForTournament(headers, tournamentIdArray);
 })();
 
-
-
-
-
 async function getTournamentIds(){
     var arrayOfTournamentIds;
     arrayOfTournamentIds = await tournamentsForLeague.getTournamentsForYearId(headers, leagueId);
-
-    // for(var i = 0; i < arrayOfTournamentIds.length; i++){
-    //     console.log(arrayOfTournamentIds[i].toString());
-    // }
 
     return arrayOfTournamentIds;
 }
